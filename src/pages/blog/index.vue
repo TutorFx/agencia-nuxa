87661e91444e6add06ab47e9c2f2f7efc6d4c217
TutorFx <gabriel_serejo@live.com>
2023-09-19 18:57:41 -0300

    :mag_right: Resolve #6

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -45,6 +45,24 @@
 import { useRouteQuery } from '@vueuse/router'
 const search = useRouteQuery<string>('search')
 const state = ref(search.value ?? undefined)
-</script>
 
-<style></style>
+defineOgImage({
+  component: 'OGImageBlog',
+  title: 'Blog',
+  description: 'Explore Nosso Blog e Fique Por Dentro de Nossas Dicas',
+})
+useHead({
+  title: 'Blog',
+})
+useSchemaOrg([
+  defineArticle({
+    headline: 'Conecte-se Com a Inovação: Explore Nosso Blog e Fique Por Dentro de Nossas Dicas',
+    description: `
+      Em nosso blog, nos esforçamos para fornecer aos nossos leitores as informações 
+      mais recentes e relevantes no campo do desenvolvimento. Sabemos que a inovação 
+      é a chave para o sucesso em qualquer indústria, e é por isso que nos dedicamos 
+      a explorar novas ideias e compartilhá-las com você.
+    `,
+  })
+])
+</script>
--- src/pages/contato.vue
+++ src/pages/contato.vue
@@ -3,3 +3,23 @@
     <form-fields />
   </nuxa-container>
 </template>
+
+<script setup lang="ts">
+  defineOgImage({
+    component: 'OGImageBlog',
+    title: 'Entre em Contato',
+    description: 'Entre em contato com nossa equipe',
+  })
+  useHead({
+    title: 'Entre em Contato',
+  })
+  useSchemaOrg([
+    defineArticle({
+      headline: 'Entre em contato com nossa equipe',
+      description: `
+      Estamos aqui para transformar sua visão em realidade! Preencha o formulário abaixo para nos contar mais sobre o seu projeto,
+      ideias ou dúvidas. Nossa equipe dedicada está ansiosa para ouvir você e responder prontamente. Juntos, podemos criar algo incrível na web.
+      `,
+    })
+  ])
+</script>
