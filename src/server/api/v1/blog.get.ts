64d7198d6e20508ebea31c7f93e075702d8bc818
Gabriel Serejo <gabriel_serejo@live.com>
2023-09-22 19:45:12 -0300

    🐛 Fix footer issues related to blog (#22)

--- src/server/api/v1/blog.get.ts
+++ src/server/api/v1/blog.get.ts
@@ -1,16 +1,16 @@
 import { createContent } from "@/contentful"
 
 export default defineEventHandler(async (event) => {
-  const query = getQuery<{ page?: number }>(event)
+  const query = getQuery<{ page?: number, limit?: number}>(event)
   const client = createContent()
   const getPage = () => {
     if (!query.page) return 0;
-    return query.page * 20
+    return query.page * (query.limit ?? 20)
   }
   const response = await client.getEntries({ 
     content_type: 'blog', 
     skip: getPage(), 
-    limit: 20, 
+    limit: (query.limit ?? 20), 
     // @ts-expect-error - Select got an type error.
     select: 'fields.thumb, fields.title, fields.path' 
   })
