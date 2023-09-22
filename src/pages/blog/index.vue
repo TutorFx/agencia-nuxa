375464ad82132515694e80d4f42740a0ce157c89
Gabriel Serejo <gabriel_serejo@live.com>
2023-09-22 17:12:35 -0300

    💄 Add "Blog" button to menu (#19)

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,25 +1,6 @@
 <template>
   <div>
     <section>
-      <nuxa-container class="grid grid-cols-1 gap-6 lg:grid-cols-[1fr_max-content]">
-        <div />
-        <div
-          class="grid overflow-hidden max-lg:gap-6 lg:grid-cols-[1fr_min-content] lg:rounded-full lg:bg-background-400"
-        >
-          <input
-            v-model="state"
-            class="bg-background-400 px-9 py-6 text-white focus:outline-0"
-            placeholder="Pesquise um projeto..."
-            type="text"
-            @keypress.enter="search = state"
-          >
-          <nuxa-button @click.prevent="search = state">
-            Pesquisar
-          </nuxa-button>
-        </div>
-      </nuxa-container>
-    </section>
-    <section>
       <nuxa-container class="py-24">
         <blog-list />
       </nuxa-container>
@@ -33,10 +14,6 @@
 </template>
 
 <script setup lang="ts">
-import { useRouteQuery } from '@vueuse/router'
-const search = useRouteQuery<string>('search')
-const state = ref(search.value ?? undefined)
-
 defineOgImage({
   component: 'OGImageBlog',
   title: 'Blog',
